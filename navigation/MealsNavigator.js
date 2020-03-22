9e7d05698f90c373a3cb14e018c23971d7cf9a77
HalukYILDIZ <halukyildiz4000@gmail.com>
2020-03-22 16:36:37 +0300

    tabBar added

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,12 +1,16 @@
+import React from 'react';
+
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealScreen from '../screens/CategoryMealScreen';
-//import FavoriteScreen from '../screens/FavoriteScreen';
+import FavoriteScreen from '../screens/FavoriteScreen';
 //import FilterScreen from '../screens/FiltersScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import Colors from '../constants/Colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -25,4 +29,30 @@ const MealsNavigator = createStackNavigator(
   },
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: tabinfo => {
+          return <Icon name="cutlery" size={25} color={tabinfo.tintColor} />;
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoriteScreen,
+      navigationOptions: {
+        tabBarIcon: tabinfo => {
+          return <Icon name="star" size={25} color={tabinfo.tintColor} />;
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  },
+);
+
+export default createAppContainer(MealsFavTabNavigator);
